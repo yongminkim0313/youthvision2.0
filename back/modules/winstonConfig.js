4cf2b552cddc17212c44a897ef5db60033166e72
kimyongmin <kimyongmin1@naver.com>
2022-08-26 10:46:11 +0900

    6

--- back/modules/winstonConfig.js
+++ back/modules/winstonConfig.js
@@ -1,7 +1,7 @@
 const winston = require('winston');
 const winstonDaily = require('winston-daily-rotate-file');
 
-const logDir = 'logs';  // logs 디렉토리 하위에 로그 파일 저장
+const logDir = `${process.env.LOG_PATH}`;  // logs 디렉토리 하위에 로그 파일 저장
 const { combine, timestamp, printf } = winston.format;
 
 // Define log format
