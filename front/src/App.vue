e0a8c8c150476b0ff1cb3825a7d00a6e2d0dc0c7
kimyongmin <kimyongmin1@naver.com>
2023-02-13 17:26:16 +0900

    modified:   front/src/App.vue 	modified:   front/src/assets/kakao.css

--- front/src/App.vue
+++ front/src/App.vue
@@ -7,42 +7,33 @@
         YOUTHVISION
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-menu open-on-hover offset-y auto tile >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn elevation="0" plain color="rgba(100,100,100,0)" dark v-bind="attrs" v-on="on" class="white--text text-h5 d-none d-md-flex d-lg-flex d-xl-flex">
-            <span :class="scrollTop?'black--text':'white--text'">소개</span>
-          </v-btn>
-        </template>
-        <v-sheet class="" width="100vw" height="200px">
-          <v-list expand>
-            <v-list-item link> <v-list-item-title>준비중입니다.</v-list-item-title> </v-list-item>
-          </v-list>
-        </v-sheet>
-      </v-menu>
+      <template v-for="(menu, i) in menuList">
+        <v-menu open-on-hover offset-y auto tile :key="i">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn elevation="0" plain color="rgba(100,100,100,0)" dark v-bind="attrs" v-on="on" class="white--text text-h5 d-none d-md-flex d-lg-flex d-xl-flex">
+              <span :class="scrollTop?'black--text':'white--text'">{{ menu.title }}</span>
+            </v-btn>
+          </template>
+          <v-sheet class="" width="100vw" height="200px">
+            <v-list expand>
+              <v-list-item link v-for="(sub,si) in menu.subMenu" :key="si"> 
+                <v-list-item-title>{{ sub.subTitle }}</v-list-item-title> 
+              </v-list-item>
+            </v-list>
+          </v-sheet>
+        </v-menu>
+        <v-spacer :key="i"></v-spacer>
+      </template>
+      <div class="area_util">
+        <span @click="kakaoLogin" class="d-inline-block text-caption" :class="scrollTop?'black--text':'white--text'">로그인</span>
+        <v-divider vertical inset class="mx-2" style="height:14px; border-color:rgba(150,150,150,0.5)"></v-divider>
+        <span class="d-inline-block text-caption" :class="scrollTop?'black--text':'white--text'">문의하기</span>
+        <v-divider vertical inset class="mx-2" style="height:14px; border-color:rgba(150,150,150,0.5)"></v-divider>
+        <span class="d-inline-block text-caption" :class="scrollTop?'black--text':'white--text'">FAQ</span>
+        
+      </div>
       <v-spacer></v-spacer>
-      <v-menu open-on-hover offset-y auto tile open-delay="20">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn elevation="0" plain color="rgba(100,100,100,0)" dark v-bind="attrs" v-on="on" class="white--text text-h5 d-none d-md-flex d-lg-flex d-xl-flex">
-            <span :class="scrollTop?'black--text':'white--text'">소식</span>
-          </v-btn>
-        </template>
-        <v-sheet class="" width="100vw" height="200px">
-          <v-list expand>
-            <v-list-item link> <v-list-item-title>준비중입니다.</v-list-item-title> </v-list-item>
-          </v-list>
-        </v-sheet>
-      </v-menu>
-        <v-spacer></v-spacer>
-        <div class="area_util">
-          <span @click="kakaoLogin" class="d-inline-block text-caption" :class="scrollTop?'black--text':'white--text'">로그인</span>
-          <v-divider vertical inset class="mx-2" style="height:14px; border-color:rgba(150,150,150,0.5)"></v-divider>
-          <span class="d-inline-block text-caption" :class="scrollTop?'black--text':'white--text'">문의하기</span>
-          <v-divider vertical inset class="mx-2" style="height:14px; border-color:rgba(150,150,150,0.5)"></v-divider>
-          <span class="d-inline-block text-caption" :class="scrollTop?'black--text':'white--text'">FAQ</span>
-          
-        </div>
-        <v-spacer></v-spacer>
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="d-md-none d-lg-none d-xl-none"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="d-md-none d-lg-none d-xl-none" color="white"></v-app-bar-nav-icon>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" fixed right temporary >
@@ -54,29 +45,16 @@
       <v-list nav dense >
         <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4" >
 
-          <v-list-group no-action prepend-icon="mdi-account-circle" >
+          <v-list-group v-for="(menu,i) in menuList" :key="i" no-action prepend-icon="mdi-account-circle">
             <template v-slot:activator>
               <v-list-item-content>
-                <v-list-item-title>소개</v-list-item-title>
+                <v-list-item-title>{{ menu.title }}</v-list-item-title>
               </v-list-item-content>
             </template>
-            <v-list-item link >
-              <v-list-item-title> 준비중입니다.</v-list-item-title>
+            <v-list-item link v-for="(sub, si) in menu.subMenu" :key="si">
+              <v-list-item-title>{{ sub.subTitle }}</v-list-item-title>
               <v-list-item-icon>
-                <v-icon>mdi-bottle-tonic-plus</v-icon>
-              </v-list-item-icon>
-            </v-list-item>
-          </v-list-group>
-          <v-list-group no-action prepend-icon="mdi-account-circle" >
-            <template v-slot:activator>
-              <v-list-item-content>
-                <v-list-item-title>소식</v-list-item-title>
-              </v-list-item-content>
-            </template>
-            <v-list-item link >
-              <v-list-item-title> 준비중입니다.</v-list-item-title>
-              <v-list-item-icon>
-                <v-icon>mdi-tag</v-icon>
+                <v-icon>{{ sub.icon }}</v-icon>
               </v-list-item-icon>
             </v-list-item>
           </v-list-group>
@@ -102,6 +80,20 @@
         drawer: false,
         group: null,
         scrollTop: false,
+        menuList:[
+          {title:'소개'
+            ,subMenu:[
+              {subTitle:'준비중입니다.1', path:'/',icon:'mdi-bottle-tonic-plus'},
+              {subTitle:'준비중입니다.2', path:'/',icon:'mdi-bottle-tonic-plus'}
+            ]
+          },
+          {title:'소식'
+            ,subMenu:[
+              {subTitle:'준비중입니다.3', path:'/',icon:'mdi-bottle-tonic-plus'},
+              {subTitle:'준비중입니다.4', path:'/',icon:'mdi-bottle-tonic-plus'}
+            ]
+          },
+        ],
     }; },
     created() {
         window.addEventListener("scroll", this.isTop);
