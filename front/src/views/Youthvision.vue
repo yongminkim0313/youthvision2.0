47c213bfb63b2d9caec58cf5dbb569c023bbd880
kimyongmin <kimyongmin1@naver.com>
2023-04-05 17:46:03 +0900

    modified:   front/src/assets/camps/youthvisionInfo.svg 	modified:   front/src/views/Youthvision.vue

--- front/src/views/Youthvision.vue
+++ front/src/views/Youthvision.vue
@@ -1,9 +1,6 @@
 <template>
     <v-card color="white">
         <v-img src="../assets/about_top_bg.png" height="64" cover ></v-img>
-        <v-card max-width="80vw" class="mx-auto">
-            <p class="font-weight-bold text-h4 text-sm-h3 text-center"> 유스비전을 소개합니다 </p>
-        </v-card>
         <v-img src="../assets/camps/youthvisionInfo.svg"></v-img>
     </v-card>
 </template>
