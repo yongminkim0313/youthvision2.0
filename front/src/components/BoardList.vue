7fecf1777ef491ee1f7e29b560732d136c5d20ce
kimyongmin <kimyongmin1@naver.com>
2023-12-06 16:35:15 +0900

    modified:   back/mapper/bbs.xml 	modified:   front/src/components/BoardList.vue

--- front/src/components/BoardList.vue
+++ front/src/components/BoardList.vue
@@ -4,10 +4,9 @@
         <v-container>
             <v-row>
                 <v-col cols="12" lg="3" md="6" sm="12" v-for="board in boardList">
-                    <v-card  class="pa-2" style="height:220px; white-space: nowrap; overflow:hidden; ">
+                    <v-card @click="goBbs(board.idx)" class="pa-2" style="height:220px; white-space: nowrap; overflow:hidden; ">
                         <div style="overflow: hidden; display: inline-block; ">
                             <div style="display: inline-block;"> <p style="text-overflow: ellipsis; white-space: nowrap;">{{ board.title }}</p> </div>
-                            <v-card-actions><v-btn @click="goBbs(board.idx)">더보기</v-btn></v-card-actions>
                             <v-card-text v-html="board.contents" style="text-overflow: ellipsis;  direction: ltr;"> </v-card-text>
                         </div>
                     </v-card>
